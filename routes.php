d4d66276faab0d174fbc5df759d815e63c1241dc
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2022-08-29 12:09:59 +0100

    Minor fix
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

--- routes.php
+++ routes.php
@@ -4,6 +4,6 @@ Route::group([
     'as' => 'igniter.broadcast.auth',
     'middleware' => ['web'],
 ], function ($router) {
-    $router->post('broadcasting/auth', '\\'.\Igniter\Broadcast\Classes\Controller::class.'@auth');
-    $router->post(config('system.adminUri', '/admin').'/broadcasting/auth', '\\'.\Igniter\Broadcast\Classes\Controller::class.'@auth');
+    $router->post('broadcasting/auth', \Igniter\Broadcast\Classes\Controller::class.'@auth');
+    $router->post(config('system.adminUri', '/admin').'/broadcasting/auth', \Igniter\Broadcast\Classes\Controller::class.'@auth');
 });
