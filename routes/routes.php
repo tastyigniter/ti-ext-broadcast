6a138075e1daf97870dd18e22cbdb514e82d0bb2
Sam <6567634+sampoyigi@users.noreply.github.com>
2023-04-27 19:41:49 +0100

    Fix issue when caching routes

--- routes/routes.php
+++ routes/routes.php
@@ -3,9 +3,12 @@
 use Igniter\Flame\Igniter;
 
 Route::group([
-    'as' => 'igniter.broadcast.auth',
     'middleware' => ['web'],
 ], function ($router) {
-    $router->post('broadcasting/auth', '\\'.\Igniter\Broadcast\Classes\Controller::class.'@auth');
-    $router->post(Igniter::uri().'/broadcasting/auth', '\\'.\Igniter\Broadcast\Classes\Controller::class.'@auth');
+    $router
+        ->name('igniter.broadcast.auth')
+        ->post('broadcasting/auth', '\\'.\Igniter\Broadcast\Classes\Controller::class.'@auth');
+    $router
+        ->name('igniter.broadcast.admin.auth')
+        ->post(Igniter::uri().'/broadcasting/auth', '\\'.\Igniter\Broadcast\Classes\Controller::class.'@auth');
 });
